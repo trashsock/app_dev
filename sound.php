cbd82182dd54b339e14945c3555fef624548f48d
1900278 <e1900278@shell.vamk.fi>
2020-04-27 18:48:40 +0300

    First version

---
+++ sound.php
@@ -0,0 +1,19 @@
+<?php
+
+//$data = $_POST['data'];
+$peaks = $_POST['peaks'];
+$db_val = $_POST['db_val'];
+$today = date("Y-m-d H:i:s");
+$record = $today . ", " . $ID . ", " . $name . "\n";
+
+$file = "project.log";	//specify log file name
+if(file_exists($file))
+	$fp = fopen($file, "a");	//open file in append
+else
+	$fp = fopen($file, "w");	//else open in write
+fwrite($fp, $record);
+fclose($fp);
+echo "From Ritika Giridhar e1900278\n";
+
+?>
+
